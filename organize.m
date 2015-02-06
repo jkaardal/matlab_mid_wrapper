9daccc9bbc605d605ea4652af70f4c75442290f4
Joel Kaardal <jkaardal@gmail.com>
2015-02-06 12:30:05 -0800

    Create organize.m

---
+++ organize.m
@@ -0,0 +1,6 @@
+%% Organize output files by placing them in the results folder
+% Run this after mid has completed running
+
+system('mv *.log ./Results/');
+system('mv *.bst ./Results/');
+system('mv *.dat ./Results/');
