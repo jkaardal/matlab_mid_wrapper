4e3d145c9eaaa9cbdd674d3bce48a495a3071e28
Joel Kaardal <jkaardal@gmail.com>
2015-02-09 20:06:56 -0800

    Update submit_jobs.py

--- submit_jobs.py
+++ submit_jobs.py
@@ -16,11 +16,12 @@ for cellnum in cellnums:
 
 
 
-# Do not change anything below
+# Submission script should work without modification. 
+# Add flags if necessary after "qsub" such as "-l mem_free=4G" for a job that requires at least 4 GB of RAM.
 files = []
 for p in prefix:
   files.append("./shell_scripts/" + str(p) + "_cl.sh")
 
 for f in files:
   if os.path.isfile(f):
-    call("qsub -l mem_free=8G " + f, shell=True)
+    call("qsub " + f, shell=True)
